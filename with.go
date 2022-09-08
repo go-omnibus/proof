b683f94eb17b757c4d7977789a1ab62bc2bccb24
liuzhichun <liuzhichun@apipost.cn>
2022-09-08 14:57:19 +0800

    feat: support WithByteString

--- with.go
+++ with.go
@@ -6,6 +6,10 @@ func With(k string, v interface{}) zap.Field {
 	return zap.Any(k, v)
 }
 
+func WithByteString(k string, v []byte) zap.Field {
+	return zap.ByteString(k, v)
+}
+
 func WithError(err error) zap.Field {
 	return zap.NamedError("error", err)
 }
